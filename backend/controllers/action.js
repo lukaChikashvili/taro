1b499d321df64d63a47188a690522caa8886fef9
luka chikashvili <lukachikashvili2020@gmail.com>
2025-01-01 18:16:52 +0400

    add async middleware and user model

---
+++ backend/controllers/action.js
@@ -0,0 +1,12 @@
+const { asyncHandler } = require('../middlewares/asyncHandler')
+
+const createUser = asyncHandler(async(req, res) => {
+    res.send('hello')
+})
+
+
+
+
+module.exports = {
+    createUser
+}
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const {routes} = require("./routes/route")
 const app = express();
+
 require("dotenv").config();
 
 app.use(express.json());
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
  
 connectDB();
+
+app.use("/api", routes);
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
