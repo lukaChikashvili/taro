f3924bde883f91b7f0d8da94f7d27685c07b157a
luka chikashvili <lukachikashvili2020@gmail.com>
2025-01-01 17:48:55 +0400

    add express server

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("hello")
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
+
+
+
+
+
