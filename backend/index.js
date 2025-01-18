2ea4bc6eab47e5b5f09aa032b9aa43dd81e23ff8
luka chikashvili <lukachikashvili2020@gmail.com>
2025-01-19 03:57:46 +0400

    add cors error

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-    origin: ['https://fluent-flow.onrender.com/'],  
+    origin: ['https://fluent-flow.onrender.com'],  
       methods: 'GET,POST,PUT,DELETE',
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true
